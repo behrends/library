b46db72c3af5461b4fe0a810fc16f60e392bf7dd
Erik Behrends <erik.behrends@gmail.com>
2010-06-23 19:37:35 +0200

    updated ec to use jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,38 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-var fragmentSet = false;
+var currentQuery = "";
 
 jQuery.ajaxSetup({ 
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 
+// search 
+// jQuery.fn is a shortcut for jQuery.prototype and a prototype in javascript is like a class that can be extended
+// define a function and add it as submitWithAjax method to the jquery object's prototype
 jQuery.fn.submitWithAjax = function() {
   this.submit(function() {
     showSpinner();
-    fragmentSet = true;
-    $.setFragment({ "page" : "1", "search" : this.elements.search.value, "keyword" : "" });
+    
+    currentQuery = "search=" + this.elements.search.value;
+    
+    $("#selected").attr("id", "null");
+
+    // search --> #search=THESEARCH (remove page and keywords parameter)
+    // using jQuery BBQ: Back Button & Query Library http://benalman.com/projects/jquery-bbq-plugin/
+    $.bbq.pushState( { "search" : this.elements.search.value }, 2 );
+    
     $.get(this.action, $(this).serialize(), null, "script");
     return false;
-  })
+  });
   return this;
 };
 
 $(document).ready(function() {
+  // call function as method of an object: the function's local this keyword is bound to that object for that invocation.
   $("#search_form").submitWithAjax();
-})
-
-$(function() {  
+  
+  
+  // show book details in light box
   $("#book_entry").live("click", function() {
     $.get(this.href, function(data){
       $(data).dialog({ 
@@ -32,63 +43,74 @@ $(function() {
     });    
     return false;
   });
-  
+
+
+  // paginate
   $(".ec_pagination a").live("click", function() {
     showSpinner();
 
-    // the next line uses the jquery.ba-url plugin to enable page fragments for browser history
-    // http://benalman.com/projects/jquery-url-utils-plugin/
-    fragmentSet = true;
-    $.setFragment({ "page" : $.queryString(this.href).page });
-    
+    if($.deparam.querystring(this.href).search)
+      currentQuery = "search=" + $.deparam.querystring(this.href).search + "&page=" + $.deparam.querystring(this.href).page
+    else
+      currentQuery = $.param.querystring(this.href);
+      
+    // paginate --> #keywords[]=THEKEYWORDS&search=THESEARCH&page=NEXT/PREV (keep keywords and search parameter)
+    // using jQuery BBQ: Back Button & Query Library http://benalman.com/projects/jquery-bbq-plugin/
+    $.bbq.pushState( { "page" : $.deparam.querystring(this.href).page } );
+
     $.getScript(this.href);
     return false;
   });
-  
-  $("#category").live("click", function() {
+
+
+  // category click
+  $(".category").live("click", function() {
     showSpinner();
-
-    // the next line uses the jquery.ba-url plugin to enable page fragments for browser history
-    // http://benalman.com/projects/jquery-url-utils-plugin/
-    fragmentSet = true;
-    if($.queryString(this.href).keyword)
-      $.setFragment({ "keyword" : $.queryString(this.href).keyword, "page" : 1, "search" : "" });
-    else
-      $.setFragment({ "keyword" : "", "page" : 1, "search" : "" });
-
-    $.getScript(this.href);
     
+    currentQuery = $.param.querystring(this.href);
+
     $("#selected").attr("id", "null");
     $("li[name=" + $(this).parent().attr("name") + "]" ).attr("id", "selected");
-      
+
+    // click on category --> #keywords[]=THEKEYWORDS (remove page and search parameter)
+    // using jQuery BBQ: Back Button & Query Library http://benalman.com/projects/jquery-bbq-plugin/
+    $.bbq.pushState( { "keywords" : $.deparam.querystring(this.href).keywords }, 2 );
+  
+    $.getScript(this.href);
+
     return false;
   });
-  
-  // with jquery.ba-url plugin listen to fragment change events
-  $.fragmentChange(true);
-  // act on fragment change
-  $(document).bind("fragmentChange.page", function() {
-    // avoid to act on fragmentChange if setFragment has already been called
-    if(fragmentSet)
-      fragmentSet = false;
-    else {
-      $.getScript($.queryString(document.location.href, { 
-        "page" : $.fragment().page, 
-        "search" : $.fragment().search,
-        "keyword" : $.fragment().keyword
-        })
-      );
-      $("#selected").attr("id", "null");
+
+
+  // Bind an event to window.onhashchange that, when the history state changes,
+  // gets the url from the hash and displays either our cached content or fetches
+  // new content to be displayed.
+  $(window).bind( 'hashchange', function(e) {
+
+    // Get the hash (fragment) as a string, with any leading # removed. Note that
+    // in jQuery 1.4, you should use e.fragment instead of $.param.fragment().
+    var fragment = $.param.fragment();
+
+    if(currentQuery!=fragment) {
+      currentQuery = fragment;
+      
+      showSpinner();
+      
+      $.getScript(location.pathname + "?" + fragment);
     }
-  });
-  // this is needed for bookmarks and reloads
-  if ($.fragment().page) {
-    $(document).trigger("fragmentChange.page");
-  }
-});
+  })
+    
+
+    // Since the event is only triggered when the hash changes, we need to trigger
+    // the event now, to handle the hash the page may have loaded with.
+    $(window).trigger( 'hashchange' );
+  
+  // TODOS: fixed position for pagination bar  
+})
+
+
 
 function showSpinner(){
-  $("#pagination_top").after("<img src='../images/spinner.gif'/ class='spinner'/>");
   $("#pagination_bottom").before("<img src='../images/spinner.gif'/ class='spinner'/>");
 }
 
