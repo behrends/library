7f2e7a95c1fb76877fef9eeaf936dcbb164c0408
behrends <erik.behrends@gmail.com>
2010-06-28 00:05:00 +0200

    export dialog

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -3,5 +3,31 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function(){
-  alert('jQuery!')
+ 
+  jQuery("#export-form").dialog({
+	autoOpen: false,
+	height: 300,
+	width: 450,
+	modal: true,
+	buttons: {
+		'Export items': function() {
+			var library_id = jQuery("#library_id").val();
+			//jQuery.get("item/export", { library_id: library_id } );
+			//de.preventDefault();  //stop the browser from following
+    			window.location.href = "item/export?library_id=" + library_id;
+			jQuery(this).dialog('close');
+		},
+		Cancel: function() {
+			jQuery(this).dialog('close');
+		}
+	},
+	close: function() {}
+  });
+
+  jQuery("#item-export--link").live("click", function() {
+    jQuery('#export-form').dialog('open');
+    return false;
+  });
+
+
 });
